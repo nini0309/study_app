11fb2236de97134ab310bf7bbe8af010bc3054c1
Nishtha <nishthachitalia0309@gmail.com>
2022-11-13 17:49:09 +0530

    added datepicker widget

--- backend/forms.py
+++ backend/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
+from django.forms.widgets import SelectDateWidget
 
 from .models import Task
 
@@ -10,8 +11,12 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        
 
 class AddTask(ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'details', 'date', 'status']
+        widgets = {
+                'date': SelectDateWidget(),
+        }
